86a4e67b6a20887a0ba6430f3dcc942f644270e8
PavitarSharma <pavitarsharma144@gmail.com>
2022-08-19 11:35:06 +0530

    deploy netlify

--- src/App.js
+++ src/App.js
@@ -11,6 +11,3 @@ const App = () => {
 
 export default App
 
-//https://codesandbox.io/s/mui5-react-hook-form-7-multi-step-form-9idkw?file=/src/Components/Step1.js
-
-//https://medium.com/@phebianchukwurah/multistep-form-with-react-hooks-e57be1e31995
